73d8ccb736fe4f7225e58ee6de2b99791d7e838e
peyote2504 <maratovna362@gmail.com>
2017-01-22 23:37:00 +0600

    MY F

---
+++ Student/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Student
+    {
+        public string name;
+        public string surname;
+        public int age;
+        public double gpa;
+        public Student()
+        {
+            name = "Fatima";
+            surname = "Askarovna";
+            age = 18;
+            gpa = 2.56;
+        }
+
+        public Student(string name, string surname, int age, double gpa)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.gpa = gpa;
+        }
+
+        public override string ToString()
+        {
+            return name + " " + surname + " " + age + " " + gpa;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("Fatima", "Askarovna", 18, 2.56);
+            Console.WriteLine(s.ToString());
+        }
+    }
+}
