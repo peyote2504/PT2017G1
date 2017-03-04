1b346311f152c55e80c58551ab27be370fdba85b
peyote2504 <maratovna362@gmail.com>
2017-03-04 11:09:24 +0600

    recursion

---
+++ recursion/recursion/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace recursion
+{
+    class MainClass
+    {
+        static void emptySpace(int level)
+        {
+            for (int i = 0; i < level * 2; i++)
+                Console.Write(" ");
+        }
+
+        static void Ex4(string path, int level)
+        {
+            if (level > 2)
+                return;
+            try
+            {
+                DirectoryInfo directory = new DirectoryInfo(path);
+                FileInfo[] files = directory.GetFiles();
+                DirectoryInfo[] directories = directory.GetDirectories();
+
+                foreach (FileInfo file in files)
+                {
+                    emptySpace(level);
+                    Console.WriteLine(file.Name);
+                }
+                foreach (DirectoryInfo dInfo in directories)
+                {
+                    emptySpace(level);
+                    Console.WriteLine(dInfo.Name);
+                    Ex4(dInfo.FullName, level + 1);
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+    }
+}
