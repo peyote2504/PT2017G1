a151f352d665126a47a33938f5a4121b03f50cdf
peyote2504 <maratovna362@gmail.com>
2017-03-11 11:43:07 +0600

    serialization

---
+++ Serialization/Serialization/Complex.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    [Serializable]
+    class Complex
+    {
+        int a, b;
+        public Complex(int a, int b) //создаем конструктор
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public override string ToString() //переопределение функции в тюстринг
+        {
+            return this.a + "/" + this.b;
+        }
+        public static Complex operator +(Complex a, Complex b)  //выполняет операцию для двух дробей
+        {
+            return new Complex(a.a * b.b + b.a * a.b, a.b * b.b);
+        }
+    }
+}
---
+++ Serialization/Serialization/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream f = new FileStream(@"C:\Users\Asus\Desktop\121.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter bf = new BinaryFormatter();
+            Complex a = new Complex(5, 3), b = new Complex(2, 2);
+            Complex c = new Complex(1, 1);
+            bf.Serialize(f, c);
+            c = a + b;
+            Console.WriteLine(c);
+            bf = new BinaryFormatter();
+            f.Close();
+            f = new FileStream(@"C:\Users\Asus\Desktop\121.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            c = bf.Deserialize(f) as Complex;
+            Console.WriteLine(c);
+            Console.ReadKey();
+
+        }
+    }
+}
