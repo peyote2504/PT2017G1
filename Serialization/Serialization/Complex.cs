d20cd89ebd09618fc1c802d9006e7078a92d8d0c
peyote2504 <maratovna362@gmail.com>
2017-03-14 16:36:37 +0600

    serialitation

--- Serialization/Serialization/Complex.cs
+++ Serialization/Serialization/Complex.cs
@@ -6,22 +6,5 @@ using System.Threading.Tasks;
 
 namespace Serialization
 {
-    [Serializable]
-    class Complex
-    {
-        int a, b;
-        public Complex(int a, int b) //создаем конструктор
-        {
-            this.a = a;
-            this.b = b;
-        }
-        public override string ToString() //переопределение функции в тюстринг
-        {
-            return this.a + "/" + this.b;
-        }
-        public static Complex operator +(Complex a, Complex b)  //выполняет операцию для двух дробей
-        {
-            return new Complex(a.a * b.b + b.a * a.b, a.b * b.b);
-        }
-    }
+    
 }
--- Serialization/Serialization/Program.cs
+++ Serialization/Serialization/Program.cs
@@ -4,27 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
 
 namespace Serialization
 {
+    public class Complex
+    {
+        public int a, b;
+        public Complex() { }
+        public Complex(int a, int b) //создаем конструктор
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public override string ToString() //переопределение функции в тюстринг
+        {
+            return this.a + "/" + this.b;
+        }
+        public static Complex operator +(Complex a, Complex b)  //выполняет операцию для двух дробей
+        {
+            return new Complex(a.a * b.b + b.a * a.b, a.b * b.b);
+        }
+    }
     class Program
     {
+        public static void Save(int x, int y)
+        {
+            File.Delete("1.xml");
+            FileStream fs = new FileStream("1.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Complex));
+            Complex c = new Complex(x, y) ;
+            xs.Serialize(fs, c);
+            fs.Close();
+        }
+        public static void Resume ()
+        {
+            FileStream fs = new FileStream("1.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Complex));
+            Complex c = (Complex)xs.Deserialize(fs);
+            Console.WriteLine(c);
+            fs.Close();
+        }
         static void Main(string[] args)
         {
-            FileStream f = new FileStream(@"C:\Users\Asus\Desktop\121.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-            BinaryFormatter bf = new BinaryFormatter();
             Complex a = new Complex(5, 3), b = new Complex(2, 2);
-            Complex c = new Complex(1, 1);
-            bf.Serialize(f, c);
-            c = a + b;
-            Console.WriteLine(c);
-            bf = new BinaryFormatter();
-            f.Close();
-            f = new FileStream(@"C:\Users\Asus\Desktop\121.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-
-            c = bf.Deserialize(f) as Complex;
-            Console.WriteLine(c);
+            Console.WriteLine(a + b);
+            Complex c = a + b;
+            Save(c.a, c.b);
+            Resume();
             Console.ReadKey();
 
         }
